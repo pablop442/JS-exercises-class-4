fc2c5c25286b8ba5315a25d5c7e1ff0b6e9b53a4
pablop442 <pablo442@gmail.com>
2021-10-27 19:18:51 +0000

    other commit

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log("hello")
+// crear una funcion que genere una escusa aleatoria con esos datos 
+// creeis otra funcion que cuente el numero de repeticiones de letras en cada array
+// suprimir repeticiones en un array y devolver el array sin la repeticion
+// function que invierta todos los valores de el array
+// normas
+// documentado
+// prohibido la programacion declarativa
+// debe estar separado en funciones
+// no se pueden utilizar objetos
+// obligatorio programacion funcional
+// todas las variables y nombres en funcoines en ingles
+// escritura camel-case
+
+let surnames = ["10", "juan", "@12", "null", "antonioPerezDelCarmen", "abcdefghtioiasoisdjads", "Manolo", "Perez", "Soledad"];
+let escuses = ["OMG?", "What’s going on?", "How much is it?", "18", null, undefined, function(){}];
+let names= ["Jeferson", "Matilda", "R@fael", "1van", "Pep3", "Loquesea", "Fel1berto", "Pepit@", "D@M"];
+
+const letterCount=(arr)=>{
+
+    for(let i=0; arr.length;i++){
+        let count = 0
+        if(arr[i] !== "string"){
+            count=0
+        }
+        else arr[i]
+    }
+}
+var result = letterCount(surnames);
+console.log(result);
